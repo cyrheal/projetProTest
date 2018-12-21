f1ad3fa2465382290e1666cbfa36db43bfbadb80
simonot <cyrilsimonot@yahoo.fr>
2018-12-21 15:37:48 +0100

    projetProTest1

---
+++ template/footer.php
@@ -0,0 +1,10 @@
+        </div>
+      
+      <script defer src="assets/js/jquery-3.3.1.js"></script><!--en premier-->
+      <script defer src="assets/bootstrap/js/bootstrap.js"></script><!--  defer rapidité execution = fonction asynchrone-->
+      
+      <footer>
+          <p>test</p>
+      </footer>
+    </body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>ProjetPro</title>
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+        <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <img class="img-fluid" src="assets/img/header/headPink2.jpg" /><!-- img fluid = taille de l'image-->
+        </header>
+        <nav class="navbar navbar-expand-lg navbar-dark bgNavbar"><!--navbar-light = onglet noir,bgNavbar = couleur rose navbar-->
+            <a class="navbar-brand" href="index.php">Jessica Nails Beauty</a>
+            <!--burger menu en mode mobile-->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span><!--cible et control la navbar, a false pour que le menu burger soit par defaut rentré-->
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav"><!--  navbar-nav = aligne à l'horizontal les onglets-->
+                    <a class="nav-item nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="prestation.php">Prestation et tarifs</a><!--nav-item nav link pour changer la couleur de bleu à l actuel-->
+                    <a class="nav-item nav-link" href="conseil.php">Conseil</a>
+                    <a class="nav-item nav-link" href="contact.php">Contact</a>
+                    <a class="nav-item nav-link" href="agenda.php">Agenda</a>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid">
